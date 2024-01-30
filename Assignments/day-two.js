423524bb3e5496fb800e2217e8e197e0d18d5f86
Suyash (CDTS-ASSET) <suyash.lawand@gmail.com>
2024-01-30 17:03:42 +0530

    add: assignment-day-two

---
+++ Assignments/day-two.js
@@ -0,0 +1,18 @@
+let randomNumber = Math.random() * 100;
+const PI = 3.14; // Constant cant be changed
+let wifeName; // undefined
+
+
+let currentYear = 2024;
+let myBirthYear = 2001;
+
+function addTwoNumbers (a, b) {
+    return a + b;
+}
+
+function subtractTwoNumbers (a, b) {
+    return a - b;
+}
+
+
+console.log("My Age ",subtractTwoNumbers(currentYear, myBirthYear));
