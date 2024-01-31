61595fb8a05d5770483dc7ca7243466ae9b1854f
Suyash (CDTS-ASSET) <suyash.lawand@gmail.com>
2024-01-31 13:21:41 +0530

    add: day-three-assignment

---
+++ Assignments/day-three-assignment.js
@@ -0,0 +1,14 @@
+// Day-3 Assignment
+
+// Create a file and declare variables and assign string, boolean, undefined and null data types
+const myName = "Suyash Lawand";
+console.log(typeof myName); // string 
+
+const isStudent = true;
+console.log(typeof isStudent); // boolean
+
+let myCar;
+console.log(typeof myCar); // undefined
+
+let myBadDeed = null;
+console.log(typeof myBadDeed) // object
