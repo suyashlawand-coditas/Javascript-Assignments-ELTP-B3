3622d5321c349eafcb3bf5577c993ceeb8efc4dc
Suyash (CDTS-ASSET) <suyash.lawand@gmail.com>
2024-01-31 13:12:14 +0530

    add: day-two-assignment

---
+++ Assignments/day-two-assignment.js
@@ -0,0 +1,33 @@
+// DAY-2 Assignment
+
+
+
+// 1. Declare four variables without assigning values
+let carName;
+let carModel;
+let carMfdYear;
+let carType;
+
+
+// 2. Declare four variables with assigned values
+const PI = 3.14;
+const COUNTRY_CODE = 91;
+const COMPANY_NAME = 'Coditas';
+const CURRENCY_SYMBOL = 'Rs.'
+
+// 3. Declare variables to store your first name, last name, marital status, country and age in multiple lines.
+const firstName = 'Suyash Lawand';
+const lastName = 'Lawand';
+const maritialStatus = 'Single';
+const country = 'India';
+const age = 23;
+
+// 4. Declare variables to store your father name, mother name, and fatherAge in a single line
+let fatherName, motherName, fatherAge;
+
+// 5. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+const myAge = 23;
+const yourAge = 24;
+
+console.log("My Age", myAge)
+console.log("Your Age", yourAge)
